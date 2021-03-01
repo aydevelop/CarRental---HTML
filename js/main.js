aa109a51a4f97856dac3e29b1639565eaa06c020
aydevelop <ay.develop1@gmail.com>
2021-03-01 22:33:16 +0200

    reviews

--- js/main.js
+++ js/main.js
@@ -5,4 +5,11 @@ $(function () {
     autoplay: true,
     fade: true,
   })
+
+  $('.review__slider').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  })
 })
