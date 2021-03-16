fbbbd05490f5746a9c25d14f41fd4ae3a9e6981b
aydevelop <ay.develop1@gmail.com>
2021-03-16 15:31:22 +0200

    header media

--- js/main.js
+++ js/main.js
@@ -4,12 +4,16 @@ $(function () {
     dots: true,
     autoplay: true,
     fade: true,
-  })
+  });
 
   $('.review__slider').slick({
     arrows: false,
     dots: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-  })
-})
+  });
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+});
