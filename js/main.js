f77c18adf756149cb690295486912bb1110b6d1a
aydevelop <ay.develop1@gmail.com>
2021-03-16 15:57:02 +0200

    slider media

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ $(function () {
     dots: true,
     autoplay: true,
     fade: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          dots: false,
+        },
+      },
+    ],
   });
 
   $('.review__slider').slick({
